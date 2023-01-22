5dffddd40d6aa9e06f6ad1de34445d7ba283cfe2
jaydenhash02 <jaydenxian@outlook.com>
2023-01-23 01:56:35 +1100

    first commit

---
+++ movie-server/src/main/java/dev/jayden/movies/ReviewService.java
@@ -0,0 +1,30 @@
+package dev.jayden.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId) {
+        // insert returns the data you just pushed into it
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        // use template to update movie class
+        // update the reviewIds field
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
+}
